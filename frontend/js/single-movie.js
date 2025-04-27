a250ca4b8803b39c988cf35fd2373036ed19144e
soapsudo <blaz.ostoji22@gmail.com>
2025-04-27 21:58:59 +0200

    [frontend] Added the single movie page frontend.

--- frontend/js/single-movie.js
+++ frontend/js/single-movie.js
@@ -1,10 +1,40 @@
 const params = new URLSearchParams(window.location.search);
 const id = params.get('id');
 
-// if(id){
+async function loadData(id) {
     
+    try{
+        const response = await fetch('http://localhost:3000/movies/' + id);
+        const data = await response.json();
 
-// }else{
+        const title = document.getElementById('single-movie-title');
+        title.innerText = data.movie_title;
 
+        const desc = document.getElementById('desc');
+        desc.innerText = data.description;
 
-// }
+        const actors = document.getElementById('actors');
+        actors.innerText = data.actors.replaceAll(',', ', ');
+
+        const year = document.getElementById('year');
+        year.innerText = data.release_year;
+
+        const genre = document.getElementById('genre');
+        genre.innerText = data.category_name;
+
+        const director = document.getElementById('director');
+        director.innerText = data.director_name;
+
+        const score = document.getElementById('score');
+        score.innerText = data.score;
+
+        const poster = document.getElementById('poster');
+        poster.setAttribute('src', data.image);
+
+    }catch(error){
+        console.log('Error fetching this movie: ', error);
+    }
+
+}
+
+loadData(id);
